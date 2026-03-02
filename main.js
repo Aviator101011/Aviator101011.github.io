2f2478a82ac2aba8703c178915cac29b4f15515d
Aviator101011 <sawyergmoore101011@gmail.com>
2026-03-02 13:00:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //JAVASCRIPT!!!!!
 const output = document.getElementById("output");
-var text = "Hello World";
+var text = "Hello World, ";
 for (let i = 0; i < 10; i++){
    text += "Hello World, ";
 }
